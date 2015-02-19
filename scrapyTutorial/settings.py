ee30ef13ff1d756ad13d2c075fc70394944b551c
André Perotti Netto <andre.pnetto@gmail.com>
2015-02-19 14:08:39 -0200

    Commit inicial de teste do scrapy

---
+++ scrapyTutorial/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapyTutorial project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapyTutorial'
+
+SPIDER_MODULES = ['scrapyTutorial.spiders']
+NEWSPIDER_MODULE = 'scrapyTutorial.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapyTutorial (+http://www.yourdomain.com)'
